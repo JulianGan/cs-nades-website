da26468d7b18162f0a5dfc9dcd395a51f0f55f53
JulianGan <juliangan9@gmail.com>
2025-02-15 15:36:22 -0500

    updated meta info

--- meta_info.js
+++ meta_info.js
@@ -1,16 +1,15 @@
 all_maps = {
-	"Ancient": ["A-ct-smoke-from-A-main.mp4", "A-donut-smoke-from-A-main.mp4", "A-temple-smoke-from-A-main.mp4", "B-long-smoke-from-B-lane.mp4", "B-long-smoke-from-B-ruins.mp4", "B-pillar-molly-from-B-door.mp4", "B-short-smoke-from-B-door.mp4", "B-short-smoke-from-B-lane.mp4", "B-short-smoke-from-B-ruins.mp4", "cave-smoke-from-T-spawn.mp4", "mid-elbow-smoke-from-ct-spawn.mp4", "mid-flash-from-T-spawn.mp4", "mid-red-smoke-from-T-spawn.mp4", ],
-	"Anubis": ["A-cake-molly-from-A-water.mp4", "A-con-smoke-from-A-water.mp4", "A-con-smoke-from-B-ruins.mp4", "A-heaven-smoke-from-A-water.mp4", "A-site-flash-from-A-water.mp4", "A-stairs-flash-from-mid-doors.mp4", "A-water-flash-from-broken-wall.mp4", "B-cave-smoke-from-B-main.mp4", "B-main-flash-from-B-long.mp4", "B-palace-smoke-from-B-main.mp4", "B-palace-smoke-from-cave.mp4", "B-pillar-molly-from-B-main.mp4", "B-site-flash-from-B-main.mp4", "B-site-smoke-from-B-main.mp4", "bridge-flash-from-mid.mp4", "bridge-flash-from-ruins.mp4", "bridge-smoke-from-T-spawn.mp4", "cave-flash-from-A-upper.mp4", "cave-smoke-from-A-stairs.mp4", "cave-smoke-from-T-spawn.mp4", "mid-door-molly-from-bridge.mp4", ],
+	"Ancient": ["A-ct-smoke-backsite-molly-entry.mp4", "A-ct-smoke-from-A-main.mp4", "A-donut-smoke-from-outside-A.mp4", "B-long-smoke-from-B-ruins.mp4", "B-pillar-molly-from-B-door.mp4", "B-short-smoke-from-B-door.mp4", "B-short-smoke-from-B-ruins.mp4", "B-site-flash-from-B-doors.mp4", "cave-smoke-from-T-spawn.mp4", "mid-elbow-smoke-from-ct-spawn.mp4", "mid-flash-from-T-spawn.mp4", "mid-red-smoke-from-T-spawn.mp4", ],
+	"Anubis": ["A-cake-molly-from-A-water.mp4", "A-con-smoke-from-A-water.mp4", "A-con-smoke-from-B-ruins.mp4", "A-heaven-smoke-from-A-water.mp4", "A-site-flash-from-A-water.mp4", "A-stairs-flash-from-mid-doors.mp4", "A-water-flash-from-broken-wall.mp4", "B-main-flash-from-B-long.mp4", "B-palace-smoke-from-B-main.mp4", "B-palace-smoke-from-cave.mp4", "B-pillar-molly-from-B-main.mp4", "B-site-smoke-from-B-main.mp4", "bridge-flash-from-mid.mp4", "cave-smoke-from-street.mp4", "mid-door-flash-from-bridge.mp4", "mid-door-molly-from-bridge.mp4", ],
 	"Dust II": ["A-car-molly-from-A-long.mp4", "A-long-corner-smoke-from-T-spawn.mp4", "A-long-door-smoke-from-under-A.mp4", "A-short-lurk-smoke-from-A-short.mp4", "A-site-flash-from-A-short.mp4", "A-site-molly-from-A-short.mp4", "B-default-rollout-from-tunnels.mp4", "B-door-smoke-from-B-tunnels.mp4", "B-site-cubby-flash-from-B-tunnel.mp4", "B-window-smoke-from-B-main.mp4", "ct-cross-smoke-from-A-long.mp4", "ct-spawn-smoke-from-A-short.mp4", "ct-spawn-smoke-from-mid-xbox.mp4", "mid-door-smoke-from-T-spawn.mp4", "mid-flash-from-ct-spawn.mp4", ],
-	"Inferno": ["A-deep-long-smoke-from-mid.mp4", "A-library-smoke-from-mid.mp4", "A-long-smoke-from-second-mid.mp4", "A-moto-smoke-from-A-short.mp4", "A-short-flash-from-mid.mp4", "B-coffin-smoke-from-banana.mp4", "B-ct-boost-smoke-from-banana.mp4", "B-ct-smoke-from-banana.mp4", "B-newbox-molly-from-banana.mp4", "B-site-flash-from-banana.mp4", "mid-smoke-from-ct-spawn.mp4", "mid-upper-smoke-from-ct-spawn.mp4", ],
+	"Inferno": ["A-deep-long-smoke-from-mid.mp4", "A-library-smoke-from-mid.mp4", "A-long-smoke-from-second-mid.mp4", "A-moto-smoke-from-A-short.mp4", "A-short-flash-from-mid.mp4", "B-coffin-molly-from-banana.mp4", "B-coffin-smoke-from-banana.mp4", "B-ct-smoke-from-banana.mp4", "B-dark-molly-from-banana.mp4", "B-lurk-smoke-from-banana.mp4", "B-newbox-molly-from-banana.mp4", "B-site-flash-from-banana.mp4", "mid-smoke-from-ct-spawn.mp4", ],
 	"Mirage": [],
 	"Nuke": [],
-	"Train": ["A-olof-to-A-backsite-molly.mp4", "T-spawn-to-A-ivy-smoke.mp4", "T-spawn-to-A-site-smoke.mp4", ],
+	"Train": ["A-back-site-molly-from-olof.mp4", "A-ivy-smoke-from-T-spawn.mp4", "A-site-smoke-from-T-spawn.mp4", "B-site-flash-molly-from-heaven.mp4", "B-upper-lurk-smoke-from-heaven.mp4", "z-smoke-from-heaven.mp4", ],
 	}
 all_maps_descriptions = {
 	"Ancient": {
-		"B-long-smoke-from-B-lane.txt": "left-right click jump throw",
-		"B-short-smoke-from-B-lane.txt": "left-right click jump throw",
+		"A-donut-smoke-from-outside-A.txt": "crouch + W + left click throw",
 		},
 	"Anubis": {
 		"A-stairs-flash-from-mid-doors.txt": "left-right click jump throw",
@@ -24,14 +23,15 @@ all_maps_descriptions = {
 		},
 	"Inferno": {
 		"A-short-flash-from-mid.txt": "shift walk + left click throw",
-		"B-coffin-smoke-from-banana.txt": "left-right click jump throw, align with roof tiles",
-		"B-ct-boost-smoke-from-banana.txt": "difficult to aim consistently, do not recommend",
 		},
 	"Mirage": {
 		},
 	"Nuke": {
 		},
 	"Train": {
+		"B-site-flash-molly-from-heaven.txt": "1. left click jump throw
+2. left-right click jump throw
+3. left click jump throw",
 		},
 	}
 all_player_info = {
